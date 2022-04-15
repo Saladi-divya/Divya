6e8f343d04045ff93f1cfd298f7d2ae3e54eda1e
Divya Sri <85440375+Saladi-divya@users.noreply.github.com>
2022-04-15 20:27:20 +0530

    Create Employee.cs

---
+++ Task2Apr13/Models/Employee.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace WEBAPI_TASK2.Models
+{
+    public partial class Employee
+    {
+        public int Eid { get; set; }
+        public string Ename { get; set; }
+        public string Edesign { get; set; }
+        public decimal? Esal { get; set; }
+        public int? Did { get; set; }
+
+        public virtual Dept D { get; set; }
+    }
+}
