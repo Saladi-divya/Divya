2ce9cc712d6e6d0ec01c534eda6d72b901f2a0a6
Divya Sri <85440375+Saladi-divya@users.noreply.github.com>
2022-04-15 20:28:40 +0530

    Create Productdetails.cs

---
+++ Task2Apr13/Models/Productdetails.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace WEBAPI_TASK2.Models
+{
+    public partial class Productdetails
+    {
+        public int Pcode { get; set; }
+        public string Pname { get; set; }
+        public int Unitprice { get; set; }
+        public string Pdesc { get; set; }
+        public string Category { get; set; }
+        public int StockinHand { get; set; }
+    }
+}
