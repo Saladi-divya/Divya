a62311dafae0f9d08e284b450633bd867fe8cd90
Divya Sri <85440375+Saladi-divya@users.noreply.github.com>
2022-04-15 20:29:31 +0530

    Create Student.cs

---
+++ Task2Apr13/Models/Student.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace WEBAPI_TASK2.Models
+{
+    public partial class Student
+    {
+        public int? Strollno { get; set; }
+        public string Stname { get; set; }
+        public string Stmailid { get; set; }
+    }
+}
